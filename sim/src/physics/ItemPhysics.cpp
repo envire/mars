58ce85b165128c699d226e2e4bdd1ae435212966
Raul Dominguez <raul.dominguez@dfki.de>
2015-07-21 17:51:02 +0200

    Some first tests using the ItemPhysics class, templated from Item from envire core.

--- sim/src/physics/ItemPhysics.cpp
+++ sim/src/physics/ItemPhysics.cpp
@@ -3,13 +3,12 @@
 
 using namespace std;
 
-//void mars::sim::DummyClass::welcome()
-//{
-//    cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
-//}
-//
-//void mars::sim::ItemPhysics::hello()
-//{
-//    cout << "You successfully Inherited from a class template. Hello!" << endl;
-//}
-//
+void mars::sim::ItemPhysics::hello()
+{
+    cout << "You successfully Inherited from a class template. Hello!" << endl;
+}
+
+void mars::sim::DummyClass::welcome()
+{
+    cout << "This is a method of the class to which the template was adapted" << endl;
+}
--- sim/test/test_ItemPhysics.cpp
+++ sim/test/test_ItemPhysics.cpp
@@ -1,18 +1,29 @@
 #include <boost/test/unit_test.hpp>
 #include <physics/ItemPhysics.cpp>
+#include <envire_core/Item.hpp>
 
 using namespace mars::sim;
 
 BOOST_AUTO_TEST_CASE(contructors)
 {
     mars::sim::ItemPhysics item;
-    std::cout << "Hello World\n";
+    std::cout << "Constructor Test";
     
 }
 
-//BOOST_AUTO_TEST_CASE(it_should_not_crash_when_welcome_is_called)
-//{
-//    mars::sim::ItemPhysics item;
-////    item.welcome();
-//    item.hello();
-//}
+BOOST_AUTO_TEST_CASE(hello_world)
+{
+    mars::sim::ItemPhysics item;
+    item.hello();
+}
+
+BOOST_AUTO_TEST_CASE(dummy_welcome)
+{
+    mars::sim::ItemPhysics item;
+    mars::sim::DummyClass dummy;
+    dummy.welcome();
+    item.setData(dummy);
+    mars::sim::DummyClass dummy2 = item.getData();
+    dummy2.welcome();
+}
+
