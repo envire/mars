0b322392a774574687ca5582fbf8150db421a53b
Raul Dominguez <raul.dominguez@dfki.de>
2015-08-24 15:15:40 +0200

    Test of get and sets for the PhysicsItem class.

--- sim/test/test_ItemPhysics.cpp
+++ sim/test/test_ItemPhysics.cpp
@@ -19,12 +19,17 @@ BOOST_AUTO_TEST_CASE(hello_world)
     std::cout << "Hello World Test" << std::endl;
 }
 
-//BOOST_AUTO_TEST_CASE(set_get)
-//{
-//    mars::sim::MarsItem<mars::sim::NodePhysics> item;
-//    mars::sim::NodePhysics node(NULL);
-//    item.setData(node);
-//    std::cout << "Set data done" << std::endl;
-//    mars::sim::NodePhysics node_copy = item.getData();
-//    std::cout << "Get data done" << std::endl;
-//}
+BOOST_AUTO_TEST_CASE(set_get)
+{
+    mars::sim::WorldPhysics physics(NULL);
+    mars::sim::PhysicsItem item(&physics);
+    mars::sim::WorldPhysics physics2(NULL);
+    mars::sim::NodePhysics node(&physics2);
+    item.setData(node);
+    std::cout << "Set data done" << std::endl;
+    BOOST_ASSERT(item.getData() == node);
+    std::cout << "Get data done" << std::endl;
+    mars::sim::NodePhysics otherNode(&physics);
+    BOOST_ASSERT(item.getData() != otherNode);
+}
+/*
