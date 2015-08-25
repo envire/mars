379e1651ae7933855dd937469be4a3de780cf5e2
Raul Dominguez <raul.dominguez@dfki.de>
2015-08-25 11:04:57 +0200

    Included in the tests an example taken from osg tutorials to visualize a piramid.

--- sim/test/test_ItemPhysics.cpp
+++ sim/test/test_ItemPhysics.cpp
@@ -43,10 +43,63 @@ BOOST_AUTO_TEST_CASE(first_visualization)
     mars::sim::PhysicsItem item(&physics);
     // Somehow visualize
     osgviz::OsgViz *osgViz = osgviz::OsgViz::getInstance();
-    /*
     //load lib with some helpful primitives
     osgviz::PrimitivesFactory *primitivesfactory = osgViz->getVisualizerPlugin< osgviz::PrimitivesFactory >("PrimitivesFactory");
     osg::ref_ptr<osgviz::Object> grid = primitivesfactory->createGrid();
+    // Piramid example
+    osg::Group* root = new osg::Group();
+    osg::Geode* pyramidGeode = new osg::Geode();
+    osg::Geometry* pyramidGeometry = new osg::Geometry();
+    pyramidGeode->addDrawable(pyramidGeometry); 
+    root->addChild(pyramidGeode);
+    osg::Vec3Array* pyramidVertices = new osg::Vec3Array;
+    pyramidVertices->push_back( osg::Vec3( 0, 0, 0) ); // front left
+    pyramidVertices->push_back( osg::Vec3(10, 0, 0) ); // front right
+    pyramidVertices->push_back( osg::Vec3(10,10, 0) ); // back right 
+    pyramidVertices->push_back( osg::Vec3( 0,10, 0) ); // back left 
+    pyramidVertices->push_back( osg::Vec3( 5, 5,10) ); // peak
+    pyramidGeometry->setVertexArray( pyramidVertices );
+    osg::DrawElementsUInt* pyramidBase = 
+        new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, 0);
+    pyramidBase->push_back(3);
+    pyramidBase->push_back(2);
+    pyramidBase->push_back(1);
+    pyramidBase->push_back(0);
+    pyramidGeometry->addPrimitiveSet(pyramidBase); 
+    osg::DrawElementsUInt* pyramidFaceOne = 
+        new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
+    pyramidFaceOne->push_back(0);
+    pyramidFaceOne->push_back(1);
+    pyramidFaceOne->push_back(4);
+    pyramidGeometry->addPrimitiveSet(pyramidFaceOne);
+    osg::DrawElementsUInt* pyramidFaceTwo = 
+        new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
+    pyramidFaceTwo->push_back(1);
+    pyramidFaceTwo->push_back(2);
+    pyramidFaceTwo->push_back(4);
+    pyramidGeometry->addPrimitiveSet(pyramidFaceTwo);
+    osg::DrawElementsUInt* pyramidFaceThree = 
+        new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
+    pyramidFaceThree->push_back(2);
+    pyramidFaceThree->push_back(3);
+    pyramidFaceThree->push_back(4);
+    pyramidGeometry->addPrimitiveSet(pyramidFaceThree);
+    osg::DrawElementsUInt* pyramidFaceFour = 
+        new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLES, 0);
+    pyramidFaceFour->push_back(3);
+    pyramidFaceFour->push_back(0);
+    pyramidFaceFour->push_back(4);
+    pyramidGeometry->addPrimitiveSet(pyramidFaceFour);
+    osg::Vec4Array* colors = new osg::Vec4Array;
+    colors->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f) ); //index 0 red
+    colors->push_back(osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f) ); //index 1 green
+    colors->push_back(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f) ); //index 2 blue
+    colors->push_back(osg::Vec4(1.0f, 1.0f, 1.0f, 1.0f) ); //index 3 white 
+    colors->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f) ); //index 4 red
+    pyramidGeometry->setColorArray(colors);
+    pyramidGeometry->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+    // Piramid end
+    osgViz->addChild(root);
     osgViz->addChild(grid);
     osgViz->createWindow();
     osgViz->startThread();
@@ -58,7 +111,6 @@ BOOST_AUTO_TEST_CASE(first_visualization)
         sleep(1);
     }
     delete osgViz;
-    */
 }
 /*
  * This is the goal test 
