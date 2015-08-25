ccfa9cab18f4e38a2a3ccc84c809c7848afd1599
Raul Dominguez <raul.dominguez@dfki.de>
2015-08-25 16:06:57 +0200

    Test for diplaying objects using osg.

--- sim/test/test_ItemPhysics.cpp
+++ sim/test/test_ItemPhysics.cpp
@@ -46,6 +46,8 @@ BOOST_AUTO_TEST_CASE(first_visualization)
     //load lib with some helpful primitives
     osgviz::PrimitivesFactory *primitivesfactory = osgViz->getVisualizerPlugin< osgviz::PrimitivesFactory >("PrimitivesFactory");
     osg::ref_ptr<osgviz::Object> grid = primitivesfactory->createGrid();
+    osgViz->addChild(grid);
+    /*
     // Piramid example
     osg::Group* root = new osg::Group();
     osg::Geode* pyramidGeode = new osg::Geode();
@@ -98,9 +100,37 @@ BOOST_AUTO_TEST_CASE(first_visualization)
     colors->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f) ); //index 4 red
     pyramidGeometry->setColorArray(colors);
     pyramidGeometry->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
-    // Piramid end
     osgViz->addChild(root);
-    osgViz->addChild(grid);
+    // Piramid end
+    */
+    /*
+    // Box shape example
+    //Declare a group to act as root node of a scene:
+    osg::Group* root = new osg::Group();
+   
+    // Declare a box class (derived from shape class) instance
+    // This constructor takes an osg::Vec3 to define the center
+    // and a float to define the height, width and depth.
+    // (an overloaded constructor allows you to specify unique
+    // height, width and height values.)
+    osg::Box* unitCube = new osg::Box( osg::Vec3(0,0,0), 1.0f);
+   
+    // Declare an instance of the shape drawable class and initialize 
+    // it with the unitCube shape we created above.
+    // This class is derived from 'drawable' so instances of this
+    // class can be added to Geode instances.
+    osg::ShapeDrawable* unitCubeDrawable = new osg::ShapeDrawable(unitCube); // I have to include osg/ShapeDrawable
+   
+    // Declare a instance of the geode class: 
+    osg::Geode* basicShapesGeode = new osg::Geode();
+   
+    // Add the unit cube drawable to the geode:
+    basicShapesGeode->addDrawable(unitCubeDrawable);
+   
+    // Add the goede to the scene:
+    root->addChild(basicShapesGeode);
+    osgViz->addChild(root);
+    */
     osgViz->createWindow();
     osgViz->startThread();
     //or don't start the thread and update manually using osgViz->update();
