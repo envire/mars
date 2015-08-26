054bd37c083ff58fdbe1e450fbdf716463e7df93
Raul Dominguez <raul.dominguez@dfki.de>
2015-08-26 11:18:15 +0200

    ItemPhysics and its tests. Included in the build configuration.

--- sim/src/physics/ItemPhysics.cpp
+++ sim/src/physics/ItemPhysics.cpp
@@ -6,6 +6,6 @@ using namespace std;
 template <class T>
 void mars::sim::MarsItem<T>::hello()
 {
-  cout << "You successfully .......created a templated class from a template. Hello!" << endl;
+  cout << "You successfully created a templated class from a template. Hello!" << endl;
 };
 
--- sim/test/test_ItemPhysics.cpp
+++ sim/test/test_ItemPhysics.cpp
@@ -1,40 +1,47 @@
-//#include <boost/test/unit_test.hpp>
-//#include <physics/ItemPhysics.cpp>
-//#include <envire_core/Item.hpp>
-//
-//using namespace mars::sim;
-//
-//
-//BOOST_AUTO_TEST_CASE(contructors)
-//{
-//    mars::sim::WorldPhysics physics(NULL);
-//    mars::sim::PhysicsItem item(&physics);
-//    item.hello();
-//
-//}
+#include <boost/test/unit_test.hpp>
+#include <physics/ItemPhysics.cpp>
+#include <envire_core/Item.hpp>
 
-//BOOST_AUTO_TEST_CASE(contructors)
-//{
-    //mars::sim::WorldPhysics physics(NULL);
-    //mars::sim::PhysicsItem item(&physics);
-    //std::cout << "Constructor Test" << std::endl;
-//}
+using namespace mars::sim;
 
-//BOOST_AUTO_TEST_CASE(hello_world)
-//{
-    //mars::sim::WorldPhysics physics(NULL);
-    //mars::sim::PhysicsItem item(&physics);
-    //item.hello();
-   //// item.test = 100;
-    //std::cout << "Hello World Test" << std::endl;
-//}
+BOOST_AUTO_TEST_CASE(contructors)
+{
+    mars::sim::WorldPhysics physics(NULL);
+    mars::sim::PhysicsItem item(&physics);
+    std::cout << "Constructor Test" << std::endl;
+}
 
-//BOOST_AUTO_TEST_CASE(set_get)
-//{
-    //mars::sim::MarsItem<mars::sim::NodePhysics> item;
-    //mars::sim::NodePhysics node(NULL);
-    //item.setData(node);
-    //std::cout << "Set data done" << std::endl;
-    //mars::sim::NodePhysics node_copy = item.getData();
-    //std::cout << "Get data done" << std::endl;
-//}
+BOOST_AUTO_TEST_CASE(hello_world)
+{
+    mars::sim::WorldPhysics physics(NULL);
+    mars::sim::PhysicsItem item(&physics);
+    item.hello();
+    std::cout << "Hello World Test" << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(set_get)
+{
+    mars::sim::WorldPhysics physics(NULL);
+    mars::sim::PhysicsItem item(&physics);
+    mars::sim::WorldPhysics physics2(NULL);
+    mars::sim::NodePhysics node(&physics2);
+    item.setData(node);
+    std::cout << "Set data done" << std::endl;
+    BOOST_ASSERT(item.getData() == node);
+    std::cout << "Get data done" << std::endl;
+    mars::sim::NodePhysics otherNode(&physics);
+    BOOST_ASSERT(item.getData() != otherNode);
+}
+/*
+ * This is the goal test 
+BOOST_AUTO_TEST_CASE(first_object)
+{
+    mars::sim::WorldPhysics physics(NULL);
+    mars::sim::PhysicsItem item(&physics);
+    mars::sim::NodePhysics node = item.getData();
+    ode.compute(node); //Or however is done
+    //mars::sim::SurfacePhysicsItem item(&physics);
+    //mars::sim::SurfacePhysics surface = item.getData();
+    //ode.compute(surface);
+}
+*/
