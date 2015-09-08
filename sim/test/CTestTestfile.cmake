13393eb73065947bede19fdbaaa4f747fc9aa50a
Arne Böckmann <arne.boeckmann@dfki.de>
2015-09-08 14:14:41 +0200

    Removed files that have been added by mistake

--- sim/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dfki.uni-bremen.de/yoo/rockentern/simulation/mars/sim/test
-# Build directory: /home/dfki.uni-bremen.de/yoo/rockentern/simulation/mars/sim/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(test-test_suite-cxx "/test_suite")
-ADD_TEST(test-test_ItemManager-cxx "/test_ItemManager")
