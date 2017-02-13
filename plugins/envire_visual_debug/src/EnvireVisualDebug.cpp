55e4768c989ba69e1ec950bdc2474d548a1fb135
Raul Dominguez <raul.dominguez@dfki.de>
2017-02-13 17:16:53 +0100

    envire_visual_debug plugin. With this the graph can be observed using the mars_gui

---
+++ plugins/envire_visual_debug/src/EnvireVisualDebug.cpp
@@ -0,0 +1,74 @@
+/*
+ *  Copyright 2013, DFKI GmbH Robotics Innovation Center
+ *
+ *  This file is part of the MARS simulation framework.
+ *
+ *  MARS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation, either version 3
+ *  of the License, or (at your option) any later version.
+ *
+ *  MARS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with MARS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * \file EnvireVisualDebug.cpp
+ * \author Raul (Raul.Dominguez@dfki.de)
+ * \brief Debug
+ *
+ * Version 0.1
+ */
+
+#include "EnvireVisualDebug.hpp"
+
+namespace mars {
+  namespace plugins {
+    namespace envire_visual_debug {
+
+      using namespace mars::utils;
+      using namespace mars::interfaces;
+
+      EnvireVisualDebug::EnvireVisualDebug(lib_manager::LibManager *theManager)
+        : MarsPluginTemplateGUI(theManager, "EnvireVisualDebug") {
+
+                graphWindow = new envire::viz::EnvireVisualizerWindow();
+      }
+  
+      void EnvireVisualDebug::init() {
+
+        gui->addGenericMenuAction("../EnvireVisualDebug/showEnvireGraph", 1, this);
+
+
+        graphWindow->displayGraph(control->graph, "center");
+
+      }
+
+      void EnvireVisualDebug::reset() {
+      }
+
+      EnvireVisualDebug::~EnvireVisualDebug() {
+      }
+
+
+      void EnvireVisualDebug::update(sReal time_ms) {
+
+      }
+
+      void EnvireVisualDebug::menuAction (int action, bool checked)
+      {
+	 graphWindow -> show();
+      }
+
+    } // end of namespace envire_visual_debug
+  } // end of namespace plugins
+} // end of namespace mars
+
+DESTROY_LIB(mars::plugins::envire_visual_debug::EnvireVisualDebug);
+CREATE_LIB(mars::plugins::envire_visual_debug::EnvireVisualDebug);
