843298f5b558e0dc2d971ea9c183a088dd12cecd
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2017-04-10 12:42:45 +0200

    fixed graph->source graph->getSourceVertex

--- plugins/envire_physics/src/EnvirePhysics.cpp
+++ plugins/envire_physics/src/EnvirePhysics.cpp
@@ -342,8 +342,8 @@ void EnvirePhysics::updateTree()
   treeView = control->graph->getTree(originId);
   if(treeView.crossEdges.size() > 0)
   {
-    const GraphTraits::vertex_descriptor source = control->graph->source(treeView.crossEdges[0].edge);
-    const GraphTraits::vertex_descriptor target = control->graph->target(treeView.crossEdges[0].edge);
+    const GraphTraits::vertex_descriptor source = control->graph->getSourceVertex(treeView.crossEdges[0].edge);
+    const GraphTraits::vertex_descriptor target = control->graph->getTargetVertex(treeView.crossEdges[0].edge);
     const FrameId sourceId = control->graph->getFrameId(source);
     const FrameId targetId = control->graph->getFrameId(target);
     const string msg = "Loop in tree detected: " + sourceId + " --> " + targetId +
